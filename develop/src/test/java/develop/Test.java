4504808ae77ee74dff5ced2e7f3520f3af16e826
haifeng <chenhf@shixinyun.com>
2016-07-27 20:04:39 +0800

    [add]

--- develop/src/test/java/develop/Test.java
+++ develop/src/test/java/develop/Test.java
@@ -1,7 +1,34 @@
 package develop;
 
+import redis.clients.jedis.Jedis;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import static java.util.Arrays.asList;
+
 public class Test {
 	public static void main(String[] args) {
-		System.out.println(1);
+		MongoClient mongoClient = new MongoClient();
+		MongoDatabase db = mongoClient.getDatabase("test");
+		
+		MongoCollection collection=db.getCollection("restaurants");
+		long s=System.currentTimeMillis();
+		System.out.println(s);
+		for(int i=0;i<100000;i++){
+			collection.insertOne(new Document("address",i));
+		}
+		long e=System.currentTimeMillis();
+		collection.find(new Document("address", 200000));
+		System.out.println(e);
+		System.out.println(s-e);
+		System.out.println("end");
 	}
 }
